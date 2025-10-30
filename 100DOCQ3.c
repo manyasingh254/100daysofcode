aa2e3bb85896271165bddb88e8b20c4bc6247625
Manya <manya.21740@stu.upes.ac.in>
2025-10-30 16:16:38 +0000

    Update 100DOCQ3.c

--- 100DOCQ3.c
+++ 100DOCQ3.c
@@ -1,4 +1,4 @@
-//Write a program to calculate the area and perimeter of a rectangle given its length and b
+//Write a program to calculate the area and perimeter of a rectangle given its length and breadth
 #include <stdio.h>
 int main() 
 {

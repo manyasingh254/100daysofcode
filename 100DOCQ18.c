2150475d5f7971ff6aa505195dbe355cf21ee6cf
Manya <manya.21740@stu.upes.ac.in>
2025-10-31 16:03:21 +0000

    Updated 100DOCQ19.c

--- 100DOCQ18.c
+++ 100DOCQ18.c
@@ -2,8 +2,8 @@
 //90-100: Grade A 
 //80-89: Grade B 
 //70-79: Grade C 
-60-69: Grade D 
-below 60: Grade F.
+//60-69: Grade D 
+//below 60: Grade F.
 #include <stdio.h>
 int main() {
     int per;

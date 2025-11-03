6f57f8c047c8df8eceb014c7e4bca377584ab922
Manya <manya.21740@stu.upes.ac.in>
2025-11-03 18:21:51 +0000

    Initial commit

---
+++ 100DOCQ30.c
@@ -0,0 +1,14 @@
+//Write a program to reverse a given number.
+#include <stdio.h>
+int main() {
+    int n, rev = 0;
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    while(n != 0) {
+        rev = rev * 10 + (n % 10);
+        n /= 10;
+    }
+    printf("%d", rev);
+    return 0;
+}

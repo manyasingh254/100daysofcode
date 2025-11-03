10e56bca8c6ea864e3955fd3875314800534942a
Manya <manya.21740@stu.upes.ac.in>
2025-11-03 18:08:00 +0000

    Initial commit

---
+++ 100DOCQ27.c
@@ -0,0 +1,14 @@
+//Write a program to print the sum of the first n odd numbers.
+#include <stdio.h>
+int main() {
+    int n, i, sum = 0, odd = 1;
+    printf("Enter n: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        sum += odd;
+        odd += 2;
+    }
+    printf("%d", sum);
+    return 0;
+}

7552346a0e1a490c5ed80231630519dfd52a2b06
Manya <manya.21740@stu.upes.ac.in>
2025-11-05 14:14:15 +0000

    Initial commit

---
+++ 100DOCQ38.c
@@ -0,0 +1,13 @@
+//Write a program to find the sum of digits of a number.
+#include <stdio.h>
+int main() {
+    int n, sum = 0;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    while (n != 0) {
+        sum += n % 10;
+        n /= 10;
+    }
+    printf("%d\n", sum);
+    return 0;
+}

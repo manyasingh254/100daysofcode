9d1a5197222045d1aaa3019ef0b3d1c07ee6bd07
Manya <manya.21740@stu.upes.ac.in>
2025-11-05 13:56:57 +0000

    Initial commit

---
+++ 100DOCQ36.c
@@ -0,0 +1,14 @@
+//Write a program to find the HCF (GCD) of two numbers.
+#include <stdio.h>
+int main() {
+    int a, b, hcf;
+    printf("Enter a number: ");
+    scanf("%d%d", &a, &b);
+    int min = (a < b) ? a : b;
+    for (int i = 1; i <= min; i++) {
+        if (a % i == 0 && b % i == 0)
+            hcf = i;
+    }
+    printf("%d\n", hcf);
+    return 0;
+}

51cace7a6157bdda91cf108dd215a7d814f8091c
Manya <manya.21740@stu.upes.ac.in>
2025-11-08 11:13:19 +0000

    Initial commit

---
+++ 100DOCQ63.c
@@ -0,0 +1,19 @@
+//Merge two arrays.
+#include <stdio.h>
+int main() {
+    int n1, n2;
+    printf("Enter size of first array: ");
+    scanf("%d", &n1);
+    int arr1[n1];
+    printf("Enter elements of first array: ");
+    for (int i = 0; i < n1; i++) scanf("%d", &arr1[i]);
+    printf("Enter size of second array: ");
+    scanf("%d", &n2);
+    int arr2[n2];
+    printf("Enter elements of second array: ");
+    for (int i = 0; i < n2; i++) scanf("%d", &arr2[i]);
+    printf("Merged array: ");
+    for (int i = 0; i < n1; i++) printf("%d ", arr1[i]);
+    for (int i = 0; i < n2; i++) printf("%d ", arr2[i]);
+    return 0;
+}

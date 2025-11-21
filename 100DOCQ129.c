b6ea5923653a00aae6c99c53cf5b88155b4b5c53
Manya <manya.21740@stu.upes.ac.in>
2025-11-21 17:42:16 +0000

    Initial commit

---
+++ 100DOCQ129.c
@@ -0,0 +1,25 @@
+// A file numbers.txt contains a list of integers separated by spaces. 
+// Read all integers, compute their sum and average, and print both.
+#include <stdio.h>
+int main() {
+    FILE *fp;
+    int num, sum = 0, count = 0;
+    float avg;
+    fp = fopen("numbers.txt", "r");
+    if (fp == NULL) {
+        printf("numbers.txt not found.\n");
+        return 1;
+    }
+    while (fscanf(fp, "%d", &num) == 1) {
+        sum += num;
+        count++;
+    }
+    fclose(fp);
+    if (count == 0)
+        printf("No numbers found.\n");
+    else {
+        avg = (float)sum / count;
+        printf("Sum = %d\nAverage = %.2f\n", sum, avg);
+    }
+    return 0;
+}
